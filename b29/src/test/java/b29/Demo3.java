74eeec9a99e0e6d53cbd77fb14b031e6bfeb23f1
chand <chand@DESKTOP-NO7KHIH>
2022-03-04 18:43:03 +0530

    First commit

---
+++ b29/src/test/java/b29/Demo3.java
@@ -0,0 +1,37 @@
+package b29;
+
+
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+/*How to read data from excel file?
+ * 1. required dependency--> poi & poi-ooxml
+ * 2. poi- poor obfuscation implementation
+ * 3. open the excel file
+ * 4. goto sheet 
+ * 5. goto cell & get the value
+ * 6. close the file
+ */
+public class Demo3 {
+
+	public static void main(String[] args) throws EncryptedDocumentException, IOException {
+
+		Workbook wb = WorkbookFactory.create(new FileInputStream("./Data/Book1.xlsx"));
+		//goto sheet1
+		Sheet s = wb.getSheet("sheet1");
+		//goto 1st row 1st col, get the value
+		String v=s.getRow(0).getCell(0).getStringCellValue();
+		System.out.println(v);
+		//close the xl file
+		wb.close();
+		
+	}
+}
+
